02b0dc90fa06fbe32f90f23d3ec72a69a29bc537
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-30 12:59:47 +0200

    update spinner design

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -6,16 +6,28 @@ const rotate = keyframes`
   to { transform: rotate(360deg); }
 `;
 
-
-const Spinner = styled.div`
-  width: 50px;
-  height: 50px;
-  margin: auto;
-  border: 5px solid black;
-  border-bottom: 5px solid rgba(0, 0, 0, 0);
-  border-left: 5px solid rgba(0, 0, 0, 0);
-  border-radius: 50%;
-  animation: ${rotate} 1.5s linear infinite;
+const SpinnerContainer = styled.div`
+  position: relative;
+  width: 140px;
+  margin: 140px auto;
 `;
 
-export default Spinner;
+const SpinnerPart = styled.div`
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  border: 20px solid rgba(0, 0, 0, 0);
+  border-top: 20px solid black;
+  border-radius: 50%;
+  animation: ${rotate} ${props => props.theme.speed} linear infinite;
+`;
+
+export default function Spinner() {
+  return (
+    <SpinnerContainer>
+      <SpinnerPart theme={{ speed: "1s" }} />
+      <SpinnerPart theme={{ speed: "1.5s" }} />
+      <SpinnerPart theme={{ speed: "3s" }} />
+    </SpinnerContainer>
+  )
+}
