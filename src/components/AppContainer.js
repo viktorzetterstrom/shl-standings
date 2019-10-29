d239b46bf91672a0082bbdce96e1916804ba1f2a
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-10-29 16:14:59 +0100

    add bottom margin on appcontainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -11,6 +11,7 @@ export default styled.div`
   @media (min-width: 610px) {
     width: 600px;
     margin-top: 20px;
+    margin-bottom: 100px;
     border: 1px solid rgba(0, 0, 0, .25);
     border-radius: 5px;
     box-shadow: 0 0 25px rgba(0, 0, 0, .25);
